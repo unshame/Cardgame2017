2577502eb21a34bc3ef459a28503785ba4150cba
unshame <mrunshame@gmail.com>
2016-12-31 01:20:12 +0300

    Часть локализации. Нужно перенести в js

---
+++ commonjs/loc.js
@@ -0,0 +1,39 @@
+var cardValueToChar = function(value){
+	switch(value){
+	case 11:
+		return 'J';
+	case 12:
+		return 'Q';
+	case 13:
+		return 'K';
+	case 14:
+		return 'A';
+	default:
+		return String(value);
+	}
+}
+
+var cardValueToString = function(value){
+	switch(value){
+	case 11:
+		return 'Jack';
+	case 12:
+		return 'Queen';
+	case 13:
+		return 'King';
+	case 14:
+		return 'Ace';
+	default:
+		return String(value);
+	}
+}
+
+var getSuitStrings = function(){
+	return ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+}
+
+if(exports){
+	exports.cardValueToChar = cardValueToChar;
+	exports.cardValueToString = cardValueToString;
+	exports.getSuitStrings = getSuitStrings;
+}
