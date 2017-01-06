b8b0e7c37cce06515a8e0b80688aba2f9c752db3
unshame <mrunshame@gmail.com>
2017-01-06 12:22:27 +0300

    Добавил базовый функционал для переподключения

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 
 var express = require('express')
   , app = express(app)
-  , server = require('http').createServer(app);
-var path = require('path');
-var Game = require('./serverjs/gamelogic').Game;
-var Bot = require('./serverjs/bots').Bot;
-var Player = require('./serverjs/players').Player;
+  , Server = require('http').createServer(app);
+//var path = require('path');
 
-//Добавляем трех ботов
-var players = []
+var LobbyManager = require('./serverjs/lobbyManager').LobbyManager,
+	Lobby = require('./serverjs/lobby').Lobby,
+	Game = require('./serverjs/gamelogic').Game,
+	Bot = require('./serverjs/bots').Bot,
+	Player = require('./serverjs/players').Player;
+
+//Добавляем ботов
+var players = [];
 var games = [];
 for (var n = 0; n < 5; n++) {
 	var bot = new Bot();
 	players.push(bot);
 }
 
-// Открываем доступ к 
-app.use(express.static('public'))
+// Открываем клиентам доступ к файлам 
+app.use(express.static('public'));
 
-//we'll keep clients data here
+//Тут будет информация о клиентах
 var clients = {};
 
 //Загружаем серверную библиотеку
 var Eureca = require('eureca.io');
 
 //Создаем сервер и добавляем разрешенные клиентские функции
-var Server = new Eureca.Server({allow:[
+var server = new Eureca.Server({allow:[
 	'setId',
 	'meetOpponents',
 	'recievePossibleActions',
@@ -35,64 +38,59 @@ var Server = new Eureca.Server({allow:[
 });
 
 //Подключаем eureca к нашему http серверу
-Server.attach(server);
+server.attach(Server);
 
 //Клиент подключился
-Server.onConnect(function (conn) {
+server.onConnect(function (conn) {
     console.log('New Client id=%s ', conn.id, conn.remoteAddress);
 
 	//getClient позволяет нам получить доступ к функциям на стороне клиента
-    var remote = Server.getClient(conn.id);
+    var remote = server.getClient(conn.id);
 
 	//Запоминаем информацию о клиенте
 	clients[conn.id] = {id:conn.id, remote:remote};
 
 	//Запускаем игру с ботами и игроком
-	players.push(new Player(remote, conn.id))
 	if(!games.length){
+
+		//Подключаем клиента к экземпляру игрока
+		var p = new Player(remote, conn.id);
+
+		players.push(p);
 		games.push(new Game(players));
+	}
+	else{
+		var game = games[0];
+		if(game.disconnectedPlayers.length){
+			var p = game.playersById[game.disconnectedPlayers[0]];
+			p.remote = remote;
+			p.connid = conn.id;
+			p.connected = true;
+			remote.setId(conn.id);
+			game.disconnectedPlayers.shift();
+		}
 	}
 });
 
 //Клиент отключился
-Server.onDisconnect(function (conn) {
+server.onDisconnect(function (conn) {
     console.log('Client disconnected ', conn.id);
 
 	var removeId = clients[conn.id].id;
 
+	for(var pi in players){
+		var p = players[pi];
+		if(p.connid == removeId){
+			if(!p.game){
+				delete p;
+			}
+			else{
+				p.connected = false;
+			}
+		}
+	}
+
 	delete clients[removeId];
 });
 
-//Tell clients about each other
-Server.exports.handshake = function(id)
-{
-	var enemy=clients[id]
-	for (var c in clients)
-		if (c!=id) {
-			clients[c].remote.spawnOpponent({
-				id:id
-			});
-			var cl = clients[c];
-			enemy.remote.spawnOpponent({
-				id:c
-			});
-		}
-}
-
-
-//be exposed to client side
-Server.exports.handleKeys = function (keys,id) {
-    var conn = this.connection;
-    var updatedClient = clients[conn.id];
-
-    for (var c in clients)
-    {
-        var remote = clients[c].remote;
-        if(c != id)
-        	remote.updateState(updatedClient.id, keys);
-        //keep last known state so we can send it to new connected clients
-        clients[c].laststate = keys;
-    }
-}
-
-server.listen(8000, '0.0.0.0');
+Server.listen(8000, '0.0.0.0');
