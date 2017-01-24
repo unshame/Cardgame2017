1469972f33d10ce7b276a967f0cc145c3b71944a
unshame <mrunshame@gmail.com>
2017-01-24 13:44:08 +0300

    Исправлено относительное перемещение карты и добавлены комментарии

--- public/js/card.js
+++ public/js/card.js
@@ -67,6 +67,7 @@ Card = function (options) {
 	this.setValue(this.options.suit, this.options.value);
 };
 
+//Устанавливает значение карты 
 Card.prototype.setValue = function(suit, value){
 	if(suit === null || suit === undefined){
 		this.suit = null;
@@ -103,76 +104,121 @@ Card.prototype.setValue = function(suit, value){
 	}
 }
 
+/* ПОЗИЦИОНИРОВАНИЕ */
+
+//Устанавливает абсолютную позицию карты
 Card.prototype.setPosition = function(x, y){
 	this.sprite.x = x - this.base.x;
 	this.sprite.y = y - this.base.y;
 	this.update();
 }
 
+//Устанавливает положение карты по отношению к базе карты
 Card.prototype.setRelativePosition = function(x, y){
 	this.sprite.x = x;
 	this.sprite.y = y;
 	this.update();
 }
 
+//Устанавливает позицию базы карты
 Card.prototype.setBase = function(x, y){
 	this.base.x = x;
 	this.base.y = y;
 	this.update();
 }
 
-Card.prototype.moveTo = function(x, y, time, delay, relative, shouldRebase){
+/* /ПОЗИЦИОНИРОВАНИЕ */
 
-	relative = relative || false;
+/* ПЕРЕДВИЖЕНИЕ */
+
+/*
+ * Плавно перемещает карту
+ * @x, y Number - позиция
+ * @time Number (мс) - время перемещения
+ * @delay Number (мс) - задержка перед перемещением
+ * @relativeToBase Bool - перемещение происходит относительно базы карты
+ * @shouldRebase Bool - нужно ли перемещать базу карты или только карту
+*/
+Card.prototype.moveTo = function(x, y, time, delay, relativeToBase, shouldRebase){
+
+	relativeToBase = relativeToBase || false;
 	shouldRebase = shouldRebase || false;
 
+	//Останавливаем твин, если он есть
 	if(this.mover){
 		this.mover.stop();
 		this.mover = null;
 	}
+
 	var moveX, moveY;
+
+	//Меняем позицию базы карты перед началом анимации
+	//и меняем относительную позицию карты так, чтобы ее абсолютная позиция не менялась
 	if(shouldRebase){
+
+		//Мы будем двигать карту к новой позиции базы
 		moveX = moveY = 0;
-		var newBaseX = relative ? x + this.base.x : x;
-		var newBaseY = relative ? y + this.base.y : y;
-		var newX = this.base.x - newBaseX;
-		var newY = this.base.y - newBaseY;
+		
+		var newBaseX = relativeToBase ? x + this.base.x : x;
+		var newBaseY = relativeToBase ? y + this.base.y : y;
+		var newX = this.base.x + this.sprite.x - newBaseX;
+		var newY = this.base.y + this.sprite.y - newBaseY;
 		this.setBase(newBaseX, newBaseY);
 		this.setRelativePosition(newX,newY);
 	}
 	else{
-		moveX = relative ? x : x - this.base.x;
-		moveY = relative ? y : y - this.base.y;
+		//Если база остается прежней, то двигаем карту к нужной позиции
+		moveX = relativeToBase ? x : x - this.base.x;
+		moveY = relativeToBase ? y : y - this.base.y;
 	}
+
+	//Создаем и запускаем твин
 	this.mover = game.add.tween(this.sprite);
-	this.mover.to({x: moveX, y: moveY}, time || 0, Phaser.Easing.Quadratic.Out, true, delay || 0);
-	this.mover.onComplete.addOnce(() => {
+	this.mover.to(
+		{
+			x: moveX,
+			y: moveY
+		},
+		time || 0,
+		Phaser.Easing.Quadratic.Out,
+		true,
+		delay || 0
+	);
+	this.mover.onComplete.addOnce(function(){
 		this.mover = null;
 	}, this);
 }
 
+//Плавно возвращает карту на базу
 Card.prototype.returnToBase = function(time, delay){
 	this.moveTo(0, 0, time || 0, delay || 0, true)
 }
 
+/* /ПЕРЕДВИЖЕНИЕ */
+
+//Вызывается при нажатии на карту
 Card.prototype.mouseDown = function(sprite, pointer){
 	controller.cardClick(this, pointer);
 }
 
+//Вызывается при окончании нажатия на карту
 Card.prototype.mouseUp = function(sprite, pointer){
 	controller.cardUnclick(this, pointer);
 }
 
+//Убивает спрайты карты
 Card.prototype.kill = function() {
 	this.glow.kill();
 	this.sprite.kill();  
 }
 
+//Восстанавливает карту
 Card.prototype.reset = function(){
 	this.sprite.reset();  
 	this.setValue(this.suit, this.value);
 }
 
+//Обновляет позицию свечения
 Card.prototype.update = function() {
 	this.glow.x = this.sprite.x;
 	this.glow.y = this.sprite.y;
