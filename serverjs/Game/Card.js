1d23781e20041118f36900ccb511b7506a54642e
unshame <mrunshame@gmail.com>
2017-02-16 04:01:50 +0300

    Класс Card
    get info() - преобразовывает в обычный объект, заменяет id на cid

---
+++ serverjs/Game/Card.js
@@ -0,0 +1,21 @@
+var utils = require('../utils');
+
+class Card{
+	constructor(suit, value, spot){
+		this.id = 'card_' + utils.generateId();
+		this.suit = suit;
+		this.value = value;
+		this.spot = spot;
+	}
+	get info(){
+		let obj = {};
+		for(let key in this){
+			if(!this.hasOwnProperty(key))
+				continue;
+			obj[key == 'id' ? 'cid' : key] = this[key];
+		}
+		return obj;
+	}
+}
+
+module.exports = Card;
