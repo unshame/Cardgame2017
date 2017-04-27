09c1ee0e7e57f9bbd4de9b73b079db89dc8f020f
unshame <mrunshame@gmail.com>
2017-04-27 12:11:07 +0300

    Добавил server.js в отчет, линтинг

--- server.js
+++ server.js
@@ -3,18 +3,18 @@
  */
 
 //Node modules
-var express = require('express'),
+const express = require('express'),
 	http = require('http'),
 	Eureca = require('eureca.io');
 
 //Игровые модули
-var Game = require('./serverjs/Game/GameLogic'),
+const Game = require('./serverjs/Game/GameLogic'),
 	Bot = require('./serverjs/Players/Bot'),
-	Player = require('./serverjs/Players/Player');
+	Player = require('./serverjs/Players/Player'),
 	Tests = require('./serverjs/Tests/GameTest');
 
 //Приложение и http сервер
-var app = express(app),
+let app = express(),
 	httpServer = http.createServer(app);
 
 //Открываем клиентам доступ к файлам 
@@ -24,7 +24,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 5000));
 
 //Создаем eureca сервер и добавляем разрешенные клиентские функции
-var server = new Eureca.Server({allow:[
+let server = new Eureca.Server({allow:[
 	'setId',
 	'meetOpponents',
 	'recievePossibleActions',
@@ -37,27 +37,27 @@ var server = new Eureca.Server({allow:[
 //Прикрепляем http сервер к eureca серверу
 server.attach(httpServer);
 
-var clients = {};
-var games = [];
-var players = [];
-var randomNames = ['Lynda','Eldridge','Shanita','Mickie','Eileen','Hiedi','Shavonne','Leola','Arlena','Marilynn','Shawnna','Alanna','Armando','Julieann','Alyson','Rutha','Wilber','Marty','Tyrone','Mammie','Shalon','Faith','Mi','Denese','Flora','Josphine','Christa','Sharonda','Sofia','Collene','Marlyn','Herma','Mac','Marybelle','Casimira','Nicholle','Ervin','Evia','Noriko','Yung','Devona','Kenny','Aliza','Stacey','Toni','Brigette','Lorri','Bernetta','Sonja','Margaretta', 'Johnny Cocksucker III'];
-var newPlayers = [];
-var botsAdded = 0;
+let clients = {};
+let games = [];
+let players = [];
+let randomNames = ['Lynda','Eldridge','Shanita','Mickie','Eileen','Hiedi','Shavonne','Leola','Arlena','Marilynn','Shawnna','Alanna','Armando','Julieann','Alyson','Rutha','Wilber','Marty','Tyrone','Mammie','Shalon','Faith','Mi','Denese','Flora','Josphine','Christa','Sharonda','Sofia','Collene','Marlyn','Herma','Mac','Marybelle','Casimira','Nicholle','Ervin','Evia','Noriko','Yung','Devona','Kenny','Aliza','Stacey','Toni','Brigette','Lorri','Bernetta','Sonja','Margaretta', 'Johnny Cocksucker III'];
+let newPlayers = [];
+let botsAdded = 0;
 
-var argv = require('minimist')(process.argv.slice(2));
-var numBots = argv.b === undefined ? Number(argv.bots) : Number(argv.b);
-var numPlayers = argv.p === undefined ? Number(argv.players) : Number(argv.p);
-var rndBots = Boolean(argv.r || argv.rnd || argv.random);
-var transfer = Boolean(process.env.TRANSFER || argv.transfer);
-var testing = Boolean(argv.t || argv.test || argv.testing);
-var debug = Boolean(argv.d || argv.debug);
+let argv = require('minimist')(process.argv.slice(2));
+let numBots = argv.b === undefined ? Number(argv.bots) : Number(argv.b);
+let numPlayers = argv.p === undefined ? Number(argv.players) : Number(argv.p);
+let rndBots = Boolean(argv.r || argv.rnd || argv.random);
+let transfer = Boolean(process.env.TRANSFER || argv.transfer);
+let testing = Boolean(argv.t || argv.test || argv.testing);
+let debug = Boolean(argv.d || argv.debug);
 
 if(isNaN(numBots))
 	numBots = 3;
 if(isNaN(numPlayers) || !numPlayers)
 	numPlayers = 1;
 
-console.log('numBots=' + numBots, 'numPlayers=' + numPlayers, 'rndBots=' + rndBots, 'transfer=' + transfer, 'testing=' + testing)
+console.log('numBots=' + numBots, 'numPlayers=' + numPlayers, 'rndBots=' + rndBots, 'transfer=' + transfer, 'testing=' + testing);
 
 if(rndBots && numBots)
 	numBots = Math.floor(Math.random()*numBots) + 1;
@@ -74,21 +74,21 @@ server.onConnect(function (conn) {
 		return;
 
 	//getClient позволяет нам получить доступ к функциям на стороне клиента
-	var remote = server.getClient(conn.id);
+	let remote = server.getClient(conn.id);
 
 	//Запоминаем информацию о клиенте
 	clients[conn.id] = {id:conn.id, remote:remote};
 
 	if(!newPlayers.length && numBots){
-		var randomNamesCopy = randomNames.slice();
-		for (var n = 0; n < numBots; n++) {
-			var bot = new Bot(randomNamesCopy);
+		let randomNamesCopy = randomNames.slice();
+		for (let n = 0; n < numBots; n++) {
+			let bot = new Bot(randomNamesCopy);
 			newPlayers.push(bot);
 		}
 	}
 
 	//Подключаем клиента к экземпляру игрока	
-	var p = new Player(remote, conn.id);
+	let p = new Player(remote, conn.id);
 
 	console.log('New client %s (%s)\n', p.id, conn.id, conn.remoteAddress);
 
@@ -114,20 +114,20 @@ server.onDisconnect(function (conn) {
 
 	console.log('Client disconnected ', conn.id);
 
-	var removeId = clients[conn.id].id;
+	let removeId = clients[conn.id].id;
 
-	for(var pi = players.length - 1; pi >= 0; pi--){
-		var p = players[pi];
+	for(let pi = players.length - 1; pi >= 0; pi--){
+		let p = players[pi];
 		if(p.connId == removeId){
-			var pi = newPlayers.indexOf(p);
+			let pi = newPlayers.indexOf(p);
 			if(~pi){
 				newPlayers.splice(pi, 1);
 			}
 
 			if(!p.game){
-				var pi = players.indexOf(p);
+				let pi = players.indexOf(p);
 				if(~pi)
-					players.splice(pi, 1)
+					players.splice(pi, 1);
 			}
 			else{
 				p.connected = false;
@@ -141,17 +141,17 @@ server.onDisconnect(function (conn) {
 server.exports.recieveCompleteAction = function(action){
 	if(!action)
 		return;
-	var connId = this.connection.id;
-	var pi = players.map((p) => {return p.connId;}).indexOf(connId);
-	var player = players[pi];
+	let connId = this.connection.id;
+	let pi = players.map((p) => {return p.connId;}).indexOf(connId);
+	let player = players[pi];
 	player && player.sendResponse(action);
-}
+};
 server.exports.recieveResponse = function(){
-	var connId = this.connection.id;
-	var pi = players.map((p) => {return p.connId;}).indexOf(connId);
-	var player = players[pi];
+	let connId = this.connection.id;
+	let pi = players.map((p) => {return p.connId;}).indexOf(connId);
+	let player = players[pi];
 	player && player.sendResponse();
-}
+};
 
 //Подключаем сервер к порту
 httpServer.listen(app.get('port'), () => {
